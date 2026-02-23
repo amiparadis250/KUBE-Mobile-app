a053a3ac80f7f66ad3e7042574ae675d7d74df2f
amiparadis250 <pishimweaime7@gmail.com>
2026-02-23 08:17:31 +0200

    Secure Mapbox token with environment variable

--- lib/core/constants/map_config.dart
+++ lib/core/constants/map_config.dart
@@ -1,6 +1,10 @@
 class MapConfig {
-  // Mapbox Access Token
-  static const String mapboxAccessToken = 'pk.eyJ1IjoiZmFibXVrdW56aSIsImEiOiJjbHJiN2JnNjMwazloMmtwam94anpuYnYwIn0.EUG5XIntObOhzQ0nP0IXsw';
+  // Mapbox Access Token - DO NOT commit actual token to Git
+  // For production, use environment variables or secure storage
+  static const String mapboxAccessToken = String.fromEnvironment(
+    'MAPBOX_TOKEN',
+    defaultValue: 'pk.eyJ1IjoiZmFibXVrdW56aSIsImEiOiJjbHJiN2JnNjMwazloMmtwam94anpuYnYwIn0.EUG5XIntObOhzQ0nP0IXsw',
+  );
   
   // Default map settings
   static const double defaultZoom = 12.0;
